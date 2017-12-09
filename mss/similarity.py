52a3e8b634b3133c9e1c0cabaab6dce48c8f2c26
Ondřej Podsztavek <ondrej.podsztavek@gmail.com>
2017-12-09 20:33:02 +0100

    Add similarities vizualization

--- mss/similarity.py
+++ mss/similarity.py
@@ -1,7 +1,6 @@
 import numpy
 from sklearn.metrics.pairwise import cosine_similarity
 from .spectra import bin_spectrum
-from .db import get_db
 
 
 def bin_spectra(spectra, bins):
@@ -14,14 +13,10 @@ def bin_spectra(spectra, bins):
 
 
 # TODO experiment with bin sizes 0.1, 0.5, 1
-def detect_similar(spectra, k, bins=13000):
+def detect_similar(spectra, database, k=5, bins=13000):
     mz_matrix = bin_spectra(spectra, bins)
-    # get database
-    db_peptides, db_mz, db_binned = get_db('data/db.npz')
     # compute similarity matrix
-    similarities = cosine_similarity(mz_matrix, db_binned)
+    similarities = cosine_similarity(mz_matrix, database)
     # choose top 5 for each spectrum
     index = numpy.flip(numpy.argsort(similarities)[:, -k:], axis=-1)
-    top_peptides = db_peptides[index]
-    top_mz = db_mz[index]
-    return top_peptides, top_mz
+    return index
--- mss/web_app.py
+++ mss/web_app.py
@@ -6,6 +6,7 @@ from flask_pymongo import PyMongo
 # from werkzeug.utils import secure_filename
 from .similarity import detect_similar
 from .spectra import read_mgf
+from .db import get_db
 
 
 app.config['SECRET_KEY'] = os.urandom(24)
@@ -14,6 +15,8 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/mss'
 
 
 mongo = PyMongo(app)
+# TODO document
+PEPTIDES, MZS, DATABASE = get_db('data/db.npz')
 
 
 def allowed_file(filename, extension):
@@ -40,13 +43,12 @@ def index():
             # convert bytes file to string file
             stringio = io.StringIO(file.read().decode('utf-8'))
             spectra = read_mgf(stringio)
-            peptides, mzs = detect_similar(spectra, 1)
+            indexes = detect_similar(spectra, DATABASE, k=2)
             spectra_list = [{
-                'mz': s['m/z array'].tolist(),
-                'intensity': s['intensity array'].tolist(),
-                'peptides': p.tolist(),
-                'mzs': list(map(lambda a: a.tolist(), mz))
-                } for s, p, mz in zip(spectra, peptides, mzs)]
+                'mz': spectrum['m/z array'].tolist(),
+                'intensity': spectrum['intensity array'].tolist(),
+                'indexes': index.tolist(),
+                } for spectrum, index in zip(spectra, indexes)]
             spectra_col = mongo.db.spectra
             spectra_ids = spectra_col.insert_many(spectra_list).inserted_ids
 
@@ -70,8 +72,10 @@ def results(result_id=None):
 
 @app.route('/spectrum/<ObjectId:spectrum_id>')
 def spectrum(spectrum_id):
-    # spectrum = mongo.db.spectra.find_one({'_id': spectrum_id})
-    return render_template('spectrum.html', spectrum_id=spectrum_id)
+    spectrum = mongo.db.spectra.find_one({'_id': spectrum_id})
+    similar = [(index, PEPTIDES[index]) for index in spectrum['indexes']]
+    return render_template('spectrum.html', spectrum_id=spectrum_id,
+                           similar=similar)
 
 
 @app.route('/spectrum/<ObjectId:spectrum_id>.json')
@@ -84,4 +88,13 @@ def spectrum_json(spectrum_id):
 
 @app.route('/db/')
 def db():
-    return render_template('db.html')
+    n_spectra, bins = DATABASE.shape
+    return render_template('db.html', n_spectra=n_spectra, bins=bins)
+
+
+@app.route('/db/<int:index>.json')
+def database_json(index):
+    mzs = MZS[index]
+    mz_intensity = [{'mz': mz, 'intensity': 1}
+                    for mz in mzs]
+    return jsonify(mz_intensity)
